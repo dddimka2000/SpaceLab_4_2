28d7c7b1cf1cb901520120b2e103ef9e6bdbef11
Denis Perevertov <arrow8086527denis@gmail.com>
2023-10-14 02:28:59 -0400

    ...

--- src/main/java/org/example/service/UserDetailsServiceImpl.java
+++ src/main/java/org/example/service/UserDetailsServiceImpl.java
@@ -1,39 +1,22 @@
 package org.example.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.example.entity.UserEntity;
-import org.example.entity.UserRole;
 import org.example.repository.UserRepository;
-import org.example.security.UserDetailsImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
+@RequiredArgsConstructor
 @Log4j2
 public class UserDetailsServiceImpl implements UserDetailsService {
-    final
-    UserRepository userRepository;
 
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (username == null || username.isEmpty()) {
-            log.error("Log empty!");
-        }
-        Optional<UserEntity> userEntity = userRepository.findByEmail(username);
-        if (userEntity.isEmpty()){
-            throw new UsernameNotFoundException("User hasn't been found!");}
-        else{
-            log.info(username + " has been found");}
-
-        return new UserDetailsImpl(userEntity.get());
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
     }
 }
