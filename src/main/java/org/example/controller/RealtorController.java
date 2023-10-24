a2756c386458d11f62d5024f3a88e1580c7547be
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-10-24 03:14:02 +0300

    Почав робити вкладку ріелторів

--- src/main/java/org/example/controller/RealtorController.java
+++ src/main/java/org/example/controller/RealtorController.java
@@ -1,10 +1,12 @@
 package org.example.controller;
 
+import org.example.dto.RealtorDto;
+import org.example.entity.Realtor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/realtors")
@@ -20,13 +22,17 @@ public class RealtorController {
         return "realtors/realtor_info";
     }
 
-    @GetMapping("/edit")
-    public String editPage() {
-        return "realtors/realtor_edit";
+    @GetMapping("/add")
+    public ModelAndView editPage() {
+        ModelAndView modelAndView = new ModelAndView("realtors/realtor_add");
+        modelAndView.addObject("realtor", new RealtorDto());
+        return modelAndView;
     }
 
     @ModelAttribute
     public void activeMenuItem(Model model) {
         model.addAttribute("realtorsActive", true);
     }
+
+
 }
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,98 @@
+var contextPath = "/ProminadaDD"
+function previewImage(event, imageId) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById(imageId);
+        output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+function validateAndUpload(imageId) {
+    // Знаходимо всі інпути з класом 'fileInput'
+    const fileInputs = document.querySelectorAll('.fileInput');
+
+    // Перебираємо знайдені інпути
+    fileInputs.forEach(fileInput => {
+        const file = fileInput.files[0];
+        const toastContainer = document.createElement('div');
+        toastContainer.classList.add('position-fixed', 'bottom-0', 'end-0', 'p-3');
+        document.body.appendChild(toastContainer);
+
+        if (!file) {
+            showToast('Файл не вибрано', 'danger');
+            fileInput.value = '';
+            return;
+        }
+
+        if (file.size > 20 * 1024 * 1024) {
+            showToast('Файл перевищує 20 МБ', 'danger');
+            fileInput.value = '';
+            return;
+        }
+
+        const allowedExtensions = ['.jpg', '.jpeg', '.png', '.pdf'];
+        const fileExtension = file.name.slice(((file.name.lastIndexOf(".") - 1) >>> 0) + 2);
+
+        if (!allowedExtensions.includes(`.${fileExtension.toLowerCase()}`)) {
+            showToast('Непідтримуваний тип файлу доступні такі типи: jpg, jpeg, png, pdf', 'danger');
+            fileInput.value = '';
+            return;
+        }
+
+        showToast('Файл успішно завантажено', 'success');
+        if (imageId != null && imageId.length > 1) previewImage(event, imageId);
+    });
+}
+
+function showToast(message, type) {
+    const toastContainer = document.querySelector('.position-fixed.bottom-0.end-0.p-3');
+    const toast = document.createElement('div');
+    toast.classList.add('toast', `bg-${type}`);
+    toast.innerHTML = `
+        <div class="toast-body text-white">
+            <button type="button" class="btn-close" style="margin-left: 93%; margin-bottom: 15px" data-bs-dismiss="toast" aria-label="Close"></button>
+            ${message}
+        </div>`;
+    toastContainer.appendChild(toast);
+    const toastInstance = new bootstrap.Toast(toast, {
+         delay: 2000
+    });
+    toastInstance.show();
+    toast.querySelector('.btn-close').addEventListener('click', function () {
+        toastInstance.hide();
+    });
+}
+
+
+function branchSelect2() {
+    $('#branchSelect2').select2({
+        allowClear: true,
+        ajax: {
+            url: contextPath + '/branches/for/select',
+            dataType: 'json',
+            delay: 1500,
+            data: function (params) {
+                var number = params.page > 0 ? params.page-1 : 0;
+                return {
+                    query: params.term || '',
+                    page: number,
+                    size: 10
+                };
+            },
+            processResults: function(data, params) {
+                var results = data.content.map(function(item) {
+                    return { id: item.id, text: item.name };
+                });
+                var hasMore = data.totalPages > data.number;
+                return {
+                    results: results,
+                    pagination: {
+                        more: hasMore
+                    }
+                };
+            },
+            cache: true
+        }
+    });
+}
