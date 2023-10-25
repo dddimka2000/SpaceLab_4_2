d3ac25451db01578f2a3b4d231dd517694679161
dddimka2000 <2000-dimk@ukr.net>
2023-10-25 11:57:27 +0300

    капча-фикс редиректа и уведомления об ошибке

--- src/main/java/org/example/config/CaptchaFilter.java
+++ src/main/java/org/example/config/CaptchaFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.example.dto.RecaptchaResponse;
 import org.example.service.RecaptchaService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,23 +16,29 @@ import java.io.IOException;
 @Log4j2
 public class CaptchaFilter extends OncePerRequestFilter {
     private final RecaptchaService recaptchaService;
+
     public CaptchaFilter(RecaptchaService recaptchaService) {
         this.recaptchaService = recaptchaService;
     }
 
+    @Value("${server.servlet.context-path}")
+    String context;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getMethod().equals("POST") ) {
+        if (request.getMethod().equals("POST")) {
             log.info(response.getHeaderNames());
             String recaptcha = request.getParameter("g-recaptcha-response");
 
             RecaptchaResponse recaptchaResponse = recaptchaService.validateToken(recaptcha);
-            if(!recaptchaResponse.success()) {
+            if (!recaptchaResponse.success()) {
                 log.info("Invalid reCAPTCHA token");
-                throw new BadCredentialsException("Invalid reCaptcha token");
+
+                // Отправка перенаправления на страницу с ошибкой
+                response.sendRedirect("login?recaptchaError"); // Указать URL вашей страницы с ошибкой
+                return; // Важно прервать дальнейшее выполнение фильтра
             }
         }
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 }
