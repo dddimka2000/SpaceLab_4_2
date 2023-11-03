00ea070b4cb19b6ee20d9548f917f35c7b49659c
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-11-03 16:22:26 +0200

    закінчив розділ для змін

---
+++ src/main/java/org/example/controller/ChangeOfferController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.entity.ChangeOffer;
+import org.example.entity.Question;
+import org.example.service.ChangeOfferServiceImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.SecureRandom;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/proposals_for_changes")
+public class ChangeOfferController {
+    private final ChangeOfferServiceImpl changeOfferService;
+    @GetMapping
+    public ModelAndView index(){
+        return new ModelAndView("proposals_for_changes/changes_table");
+    }
+    @GetMapping("/get-all")
+    @ResponseBody
+    public Page<ChangeOffer> getAll(@RequestParam("page")int page){
+        return changeOfferService.getAll(page);
+    }
+    @GetMapping("/getById")
+    @ResponseBody
+    public ChangeOffer getById(@RequestParam Integer id){
+        return changeOfferService.getById(id);
+    }
+    @GetMapping("/delete/{id}")
+    public void deleteById(@PathVariable Integer id){
+        changeOfferService.deleteById(id);
+    }
+}
---
+++ src/main/java/org/example/repository/ChangeOfferRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.ChangeOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChangeOfferRepository extends JpaRepository<ChangeOffer, Integer> {
+}
