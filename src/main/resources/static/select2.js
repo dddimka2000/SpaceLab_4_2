b1c709ad5deff5a8e3d5875af898709975f9ebef
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-12-05 19:53:19 +0200

    bug fix file select2.js

--- src/main/resources/static/select2.js
+++ src/main/resources/static/select2.js
@@ -1,4 +1,5 @@
 var contextPath = "/ProminadaDD"
+
 async function translateAllSelect2() {
     $(".select2-selection__choice").each(async function (index, element) {
         try {
@@ -29,16 +30,16 @@ function branchSelect2(id, text) {
             dataType: 'json',
             delay: 1500,
             data: function (params) {
-                var number = params.page > 0 ? params.page-1 : 0;
+                var number = params.page > 0 ? params.page - 1 : 0;
                 return {
                     query: params.term || '',
                     page: number,
                     size: 10
                 };
             },
-            processResults: function(data, params) {
-                var results = data.content.map(function(item) {
-                    return { id: item.id, text: item.name };
+            processResults: function (data, params) {
+                var results = data.content.map(function (item) {
+                    return {id: item.id, text: item.name};
                 });
                 var hasMore = data.totalPages > data.number;
                 return {
@@ -51,11 +52,12 @@ function branchSelect2(id, text) {
             cache: true
         }
     })
-    if(text && id) {
+    if (text && id) {
         $('#branchSelect2').append(new Option(text.toString(), id.toString(), true, true));
         $('#branchSelect2').trigger('change');
     }
 }
+
 function realtorSelect2(id, text) {
     $('#realtorSelect2').select2({
         ajax: {
@@ -63,16 +65,16 @@ function realtorSelect2(id, text) {
             dataType: 'json',
             delay: 1500,
             data: function (params) {
-                var number = params.page > 0 ? params.page-1 : 0;
+                var number = params.page > 0 ? params.page - 1 : 0;
                 return {
                     query: params.term || '',
                     page: number,
                     size: 10
                 };
             },
-            processResults: function(data, params) {
-                var results = data.content.map(function(item) {
-                    return { id: item.id, text: item.name };
+            processResults: function (data, params) {
+                var results = data.content.map(function (item) {
+                    return {id: item.id, text: item.name};
                 });
                 var hasMore = data.totalPages > data.number;
                 return {
@@ -85,24 +87,24 @@ function realtorSelect2(id, text) {
             cache: true
         }
     });
-    if(text && id) {
+    if (text && id) {
         $('#realtorSelect2').append(new Option(text.toString(), id.toString(), true, true));
         $('#realtorSelect2').trigger('change');
     }
 }
 
 
-function roleSelect2(text, id){
+function roleSelect2(text, id) {
     $('#roleSelect2').select2({
         minimumResultsForSearch: -1,
         ajax: {
-            url: contextPath+'/enum/role',
+            url: contextPath + '/enum/role',
             dataType: 'json',
-            processResults: function(data) {
+            processResults: function (data) {
 
                 var index = 0;
-                var results = data.map(function(data) {
-                    return { id: data, text: data };
+                var results = data.map(function (data) {
+                    return {id: data, text: data};
                 });
                 return {
                     results: results
@@ -110,25 +112,26 @@ function roleSelect2(text, id){
             },
         }
     })
-    if(text && id) {
+    if (text && id) {
         $('#roleSelect2').append(new Option(text.toString(), id.toString(), true, true));
         $('#roleSelect2').trigger('change');
     }
 }
-function informationSourceSelect2(text, id){
-    var selectName='#informationSourceSelect2'
-    var url= '/enum/informationSource'
+
+function informationSourceSelect2(text, id) {
+    var selectName = '#informationSourceSelect2'
+    var url = '/enum/informationSource'
 
     $(selectName).select2({
         minimumResultsForSearch: -1,
         // placeholder: "Адмін",
         ajax: {
-            url: contextPath+url,
+            url: contextPath + url,
             dataType: 'json',
-            processResults: function(data) {
+            processResults: function (data) {
                 var index = 0;
-                var results = data.map(function(data) {
-                    return { id: data, text: data };
+                var results = data.map(function (data) {
+                    return {id: data, text: data};
                 });
                 return {
                     results: results
@@ -136,38 +139,74 @@ function informationSourceSelect2(text, id){
             },
         }
     })
-    if(text && id) {
+    if (text && id) {
         $(selectName).append(new Option(text.toString(), id.toString(), true, true));
         $(selectName).trigger('change');
     }
 }
-function forSelect2(id, text, selectName, url){
-    $(selectName).select2({
-        minimumResultsForSearch: -1,
-        // placeholder: "Елемент",
-        ajax: {
-            url: contextPath+url,
+
+function forSelect2(id, text, selectName, url) {
+    if (translationsSecondExample === undefined) {
+        $.ajax({
+            url: '/ProminadaDD/api/translations?language=' + languageSecondExample,
             dataType: 'json',
-            processResults: function(data) {
-                var results = data.map(function(data) {
-                    if(data.id)return { id: data.id, text: translateValue(data.name) };
-                    return { id: data, text: translateValue(data) };
-                });
-                return {
-                    results: results
-                };
+            success: function (newTranslations) {
+                translationsSecondExample = newTranslations;
+                $(selectName).select2({
+                    minimumResultsForSearch: -1,
+                    ajax: {
+                        url: contextPath + url,
+                        dataType: 'json',
+                        processResults: function (data) {
+                            var results = data.map(function (data) {
+                                if (data.id) return {id: data.id, text: translateValue(data.name)};
+                                return {id: data, text: translateValue(data)};
+                            });
+                            return {
+                                results: results
+                            };
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            console.log('AJAX error: ', textStatus, errorThrown);
+                        }
+                    }
+                })
+                if (text && id) {
+                    $(selectName).append(new Option(translateValue(text.toString()), id.toString(), true, true));
+                    $(selectName).trigger('change');
+                }
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log('AJAX error: ', textStatus, errorThrown);
+        })
+    } else {
+        $(selectName).select2({
+            minimumResultsForSearch: -1,
+            // placeholder: "Елемент",
+            ajax: {
+                url: contextPath + url,
+                dataType: 'json',
+                processResults: function (data) {
+                    var results = data.map(function (data) {
+                        if (data.id) return {id: data.id, text: translateValue(data.name)};
+                        return {id: data, text: translateValue(data)};
+                    });
+                    return {
+                        results: results
+                    };
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log('AJAX error: ', textStatus, errorThrown);
+                }
             }
+        })
+        if (text && id) {
+            $(selectName).append(new Option(translateValue(text.toString()), id.toString(), true, true));
+            $(selectName).trigger('change');
         }
-    })
-    if(text && id) {
-        $(selectName).append(new Option(text.toString(), id.toString(), true, true));
-        $(selectName).trigger('change');
     }
 }
+
 function forSelect2WithSearch(id, text, selectName, url, name) {
+
     $(selectName).select2({
         // placeholder: "element",
         ajax: {
@@ -175,16 +214,16 @@ function forSelect2WithSearch(id, text, selectName, url, name) {
             dataType: 'json',
             delay: 1500,
             data: function (params) {
-                var number = params.page > 0 ? params.page-1 : 0;
+                var number = params.page > 0 ? params.page - 1 : 0;
                 return {
                     query: params.term || '',
                     page: number,
                     size: 10
                 };
             },
-            processResults: function(data, params) {
-                var results = data.content.map(function(item) {
-                    return { id: item.id, text: item.name };
+            processResults: function (data, params) {
+                var results = data.content.map(function (item) {
+                    return {id: item.id, text: item.name};
                 });
                 var hasMore = data.totalPages > data.number;
                 return {
@@ -197,7 +236,7 @@ function forSelect2WithSearch(id, text, selectName, url, name) {
             cache: true
         }
     })
-    if(text && id) {
+    if (text && id) {
         $(selectName).append(new Option(text.toString(), id.toString(), true, true));
         $(selectName).trigger('change');
     }
