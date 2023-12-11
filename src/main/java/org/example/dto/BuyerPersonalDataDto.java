6b8dba40702af2f545328b212078f0b9fab88f89
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-12-11 18:52:24 +0200

    added validation

--- src/main/java/org/example/dto/BuyerPersonalDataDto.java
+++ src/main/java/org/example/dto/BuyerPersonalDataDto.java
@@ -1,5 +1,7 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.example.entity.Realtor;
 import org.example.entity.property.type.InformationSource;
@@ -12,10 +14,19 @@ import java.util.List;
 @Data
 public class BuyerPersonalDataDto {
     private Integer id;
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String surname;
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String name;
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String middleName;
+    @Size(min = 12, max = 15, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String phone;
+    @NotNull(message = "{error.field.empty}")
     private String email;
     private Realtor realtor;
     private String passport;
--- src/main/java/org/example/dto/QuestionDto.java
+++ src/main/java/org/example/dto/QuestionDto.java
@@ -1,13 +1,23 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class QuestionDto {
     private Integer id;
-
-    private String name, email, phone;
-
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
+    private String name;
+    @NotNull(message = "{error.field.empty}")
+    private String email;
+    @Size(min = 12, max = 15, message = "{error.field.size}")
+    private String phone;
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String question;
+    @Size(min = 1, max = 999, message = "{error.field.size}")
+    @NotNull(message = "{error.field.empty}")
     private String answer;
 }
