820f6c1365685c145b4711ddd09e69cfa77ac871
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-12-13 16:09:42 +0200

    added test for RecaptchaService

--- src/test/java/org/example/service/RecaptchaServiceTest.java
+++ src/test/java/org/example/service/RecaptchaServiceTest.java
@@ -1,62 +1,50 @@
 package org.example.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.example.dto.RecaptchaResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RecaptchaServiceTest {
+    @Mock
+    private RestTemplate restTemplate;
 
     @InjectMocks
     private RecaptchaService recaptchaService;
 
-    @Mock
-    private RestTemplate restTemplate;
-    @Value("${recaptcha.secretKey}")
-    private String secretKey;
-    @Value("${recaptcha.verifyUrl}")
-    private String verifyUrl;
     @Test
-    void testValidateToken() {
-        //TODO не получился тест
-
+    void validateToken_shouldReturnRecaptchaResponse() throws NoSuchFieldException, IllegalAccessException {
         // Arrange
-        String recaptchaToken = "sampleToken";
+        String recaptchaToken = "your-recaptcha-token";
+        RecaptchaResponse expectedResponse = new RecaptchaResponse(true, "timestamp", "hostname", 0.9, "action");
+        Field secretKeyField = RecaptchaService.class.getDeclaredField("secretKey");
+        secretKeyField.setAccessible(true);
+        secretKeyField.set(recaptchaService, "your-secret-key");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("secret", secretKey);
-        map.add("response", recaptchaToken);
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+        Field verifyUrlField = RecaptchaService.class.getDeclaredField("verifyUrl");
+        verifyUrlField.setAccessible(true);
+        verifyUrlField.set(recaptchaService, "your-verify-url");
 
-        RecaptchaResponse expectedResponse = new RecaptchaResponse(true, "sampleChallengeTs", "sampleHostname", 0.5, "sampleAction");
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.POST), any(), eq(RecaptchaResponse.class)))
+                .thenReturn(new ResponseEntity<>(expectedResponse, HttpStatus.OK));
 
-        // Use anyString() for the URL to avoid strict stubbing argument mismatch
-
-        when(recaptchaService.validateToken(recaptchaToken)).thenReturn(expectedResponse);
-
-        when(restTemplate.exchange(recaptchaToken, eq(HttpMethod.POST), any(HttpEntity.class), eq(RecaptchaResponse.class)))
-                .thenReturn(ResponseEntity.ok().body(expectedResponse));
         // Act
-        RecaptchaResponse result = recaptchaService.validateToken(recaptchaToken);
+        RecaptchaResponse actualResponse = recaptchaService.validateToken(recaptchaToken);
 
         // Assert
-        assertEquals(expectedResponse, result);
+        verify(restTemplate).exchange(anyString(), eq(HttpMethod.POST), any(), eq(RecaptchaResponse.class));
+        assertEquals(expectedResponse, actualResponse);
     }
-
 }
