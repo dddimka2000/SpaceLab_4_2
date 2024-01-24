42a5193072329d971f99e26726f504515db885dc
dddimka2000 <2000-dimk@ukr.net>
2024-01-24 02:56:08 +0200

    security fix

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig {
                         {
                             try {
                                 authorize.requestMatchers("/auth/login/**","/auth/login", "/auth/registration", "/auth/process_login","/img/**","/vendor/**").permitAll()
-                                        .requestMatchers("/wrrwrw/**", "/personal_account").authenticated()
+                                        .requestMatchers("/**", "/personal_account").authenticated()
                                         .anyRequest().permitAll()
                                         .and()
                                         .logout()
